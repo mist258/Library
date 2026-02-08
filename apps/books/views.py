23e684ebc899c15f128d36b0590d294ccf2822a2
mist258 <natalia.kolchuk@gmail.com>
2026-02-08 12:21:31 +0200

    completed task

--- apps/books/views.py
+++ apps/books/views.py
@@ -13,12 +13,18 @@ class BookListView(ListView):
     def get_queryset(self):
         queryset = BookModel.objects.select_related('author').all()
         q = self.request.GET.get('q')
+        sort = self.request.GET.get('sort')
 
         if q:
             queryset = queryset.filter(
                 Q(title__icontains=q) |
                 Q(author__name__icontains=q)
             )
+
+        if sort == 'asc':
+            queryset = queryset.order_by('publish_date')
+        elif sort == 'desc':
+            queryset = queryset.order_by('-publish_date')
 
         return queryset
 
